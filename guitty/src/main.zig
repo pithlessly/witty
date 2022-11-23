d8ee02b47ca04a546b0af7c93a417565444c2f35
pithlessly <79002325+pithlessly@users.noreply.github.com>
2022-11-23 11:41:08 -0800

    initialize SDL2 and create a window in guitty main

--- guitty/src/main.zig
+++ guitty/src/main.zig
@@ -1,3 +1,7 @@
+const c = @cImport({
+    @cInclude("SDL.h");
+});
+
 const witty = @import("witty");
 
 const std = @import("std");
@@ -84,7 +88,32 @@ fn getFrameStats(alloc: Allocator, file: File) !void {
     });
 }
 
+fn getSdlError() [*:0]const u8 {
+    return @ptrCast([*:0]const u8, c.SDL_GetError());
+}
+
 fn doPlayback(alloc: Allocator, file: File) !void {
+    if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
+        std.log.err("SDL initialization failed: {s}", .{ getSdlError() });
+        return error.SDLInitializationFailed;
+    }
+    defer c.SDL_Quit();
+
+    const win_width = 500;
+    const win_height = 500;
+    const win = c.SDL_CreateWindow(
+        "Witty",
+        c.SDL_WINDOWPOS_UNDEFINED,
+        c.SDL_WINDOWPOS_UNDEFINED,
+        win_width,
+        win_height,
+        c.SDL_WINDOW_OPENGL,
+    ) orelse {
+        std.log.err("Unable to create window: {s}", .{ getSdlError() });
+        return error.SDLInitializationFailed;
+    };
+    defer c.SDL_DestroyWindow(win);
+
     const Ctx = struct {
         screen: witty.Screen = .{},
         const Err = error{};
